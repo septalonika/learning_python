f7adf3a0f6474dfc2777cd5c50280368795c9c78
septalonika <andikabeatbox@gmail.com>
2025-02-27 23:21:46 +0700

    create simple welcome message using python

---
+++ session1.py
@@ -0,0 +1,8 @@
+import random
+
+welcome_message = "Welcome to Guess the Glass"
+ball_position = random.randint(1, 10)
+
+print("=========================================")
+print(f"====== {welcome_message} =======")
+print("=========================================")
