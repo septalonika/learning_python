d7ffb2a9d630b0efe95d7704c55f4259f0985000
septalonika <andikabeatbox@gmail.com>
2025-02-27 23:36:28 +0700

    rename session1.py to guess_the_glass.py

--- session1.py
+++
@@ -1,8 +0,0 @@
-import random
-
-welcome_message = "Welcome to Guess the Glass"
-ball_position = random.randint(1, 10)
-
-print("=========================================")
-print(f"====== {welcome_message} =======")
-print("=========================================")
