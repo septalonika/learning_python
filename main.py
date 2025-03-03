0410c0fe5ac71fcb409ac2b096dd6ba24e339004
septalonika <andikabeatbox@gmail.com>
2025-03-03 14:09:11 +0700

    importing scheme

---
+++ main.py
@@ -0,0 +1,4 @@
+import my_script
+
+print("Kode di main.py")
+my_script.my_function()
---
+++ my_script.py
@@ -0,0 +1,8 @@
+def my_function():
+    print("Fungsi dari my_script.py")
+
+print("Kode di luar if statement di my_script.py")
+
+if __name__ == '__main__':
+    print("Kode di dalam if __name__ == '__main__': di my_script.py")
+    my_function()
