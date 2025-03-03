6a1b577d61c1d6fea4a1a88d6e49907257e5dc8e
septalonika <andikabeatbox@gmail.com>
2025-03-03 16:20:50 +0700

    learning array in python

---
+++ array_manipulating.py
@@ -0,0 +1,11 @@
+first_array = [1,2,3,4,5,6,7,8,9,10]
+
+temp_array = first_array.copy()
+
+temp_array[2] = 'uhuy'
+
+# temp_array = first_array
+# temp_array[1] = 20 sama kaya javascript ini ngerubah array asli juga
+
+print("first_array", first_array)
+print("temp_array", temp_array) 
